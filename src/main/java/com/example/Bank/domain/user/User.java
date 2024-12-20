1e6e90ffcef460ccec78020fa8ed402b71ecca1c
postwo <codo7717@naver.com>
2024-12-20 17:15:24 +0900

    user 테이블 생성과 auditing 설정 완료

---
+++ src/main/java/com/example/Bank/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.example.Bank.domain.user;
+
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_tb")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 60) //패스워드 인코딩
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserEnum role; //admin, customer
+
+    @CreatedDate //insert
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //insert, update
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
