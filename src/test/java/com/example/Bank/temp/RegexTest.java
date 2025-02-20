b24f3571288a2df6b0eab2d45f068f1c1961b44a
postwo <codo7717@naver.com>
2025-02-21 05:01:54 +0900

    정규표현식 실습2

--- src/test/java/com/example/Bank/temp/RegexTest.java
+++ src/test/java/com/example/Bank/temp/RegexTest.java
@@ -10,6 +10,7 @@ public class RegexTest {
     //정규표현식
     // ^ = ex) 한글로 시작하는것
     // $ = ex) 한글로 끝나는것
+    // [^] = ex) [^가-힣] 영어로 치환
     @Test
     public void 한글만된다_test() throws Exception {
         String value = "한글";
@@ -17,53 +18,39 @@ public class RegexTest {
         System.out.println("테스트 :"+result);
     }
 
+    //가-힣 이것만 넣으면 ㄴ ㅁ ㄱ 이렇게 넣으면 false 가뜬다 하지만 ㄱ-ㅎ를 추가하면 ㄴ ㅁ ㄱ 을 넣어도 true값이 나온다
     @Test
     public void 한글만안된다_test() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+        String value = "abc";
+        Boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$",value);
+        System.out.println("테스트 :"+result);
     }
 
     @Test
     public void 영어만된다_test() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+        String value = "s";
+        Boolean result = Pattern.matches("^[a-zA-Z]+$",value);
+        System.out.println("테스트 :"+result);
     }
 
     @Test
     public void 영어는안된다_test() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+        String value = "가s";
+        Boolean result = Pattern.matches("^[^a-zA-Z]*$",value);
+        System.out.println("테스트 :"+result);
     }
 
     @Test
     public void 영어와숫자만된다_test() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+        String value = "ssar123";
+        Boolean result = Pattern.matches("^[a-zA-Z0-9]+$",value);
+        System.out.println("테스트 :"+result);
     }
 
     @Test
     public void 영어만되고_길이는최소2최대4이다_test() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+        String value = "ssa";
+        Boolean result = Pattern.matches("^[a-zA-Z]{2,4}$",value);
+        System.out.println("테스트 :"+result);
     }
 }
