1380cd83462eacdbd8c114c7fc4f71c89c56da91
postwo <codo7717@naver.com>
2025-02-21 05:35:26 +0900

    정규표현식 DTO 적용

--- src/main/java/com/example/Bank/dto/user/UserReqDto.java
+++ src/main/java/com/example/Bank/dto/user/UserReqDto.java
@@ -3,6 +3,8 @@ package com.example.Bank.dto.user;
 import com.example.Bank.domain.user.User;
 import com.example.Bank.domain.user.UserEnum;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,13 +14,23 @@ public class UserReqDto {
     @Setter
     @Getter
     public static class JoinReqDto{
-        @NotEmpty //null,공백 일 수  없다
+
+        @NotEmpty //null,공백 일 수 없다
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message ="영문/숫자 2~20자 이내로 작성해주세요")
         private String username;
+
         @NotEmpty
+        @Size(min = 4, max =20) // 사이즈 스트링전용
         private String password;
+
+
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message ="이메일 형식으로 작성해주세요")
         private String email;
+
+
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message ="한글/영문 1~20 자 이내로 작성해주세요")
         private String fullname;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder){
